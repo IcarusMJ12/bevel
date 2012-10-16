c7b72dcc65f716a86c5e8db2572cdbd36f17928c
Igor Kaplounenko <igor@bittorrent.com>
2012-10-16 14:53:06 -0700

    added the BSD license

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@
 
 from setuptools import setup
 
-setup(name='bevel',
-        version='0.1.0',
-        description='a selection of tools for viewing and editing bencoded entities',
-        author='Igor Kaplounenko',
-        author_email='igor@bittorrent.com',
+setup(name = 'bevel',
+        version = '0.1.0',
+        description = 'a selection of tools for viewing and editing bencoded entities',
+        author = 'Igor Kaplounenko',
+        author_email = 'igor@bittorrent.com',
         license = 'BSD',
         keywords = 'bevel bencode bittorrent utorrent',
         provides = ['bevel'],
         py_modules = ['bevel'],
         scripts = ['bcat', 'bgrep', 'bsed'],
-        long_description=open('README').read()
+        long_description = open('README').read()
 )
