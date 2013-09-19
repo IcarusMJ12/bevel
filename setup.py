776875da797c0861bb90a83c502e492535d1c4f2
Igor Kaplounenko <igor@bittorrent.com>
2013-09-19 16:21:28 -0700

    added installation instructions and a bencode dependency to setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@
 from setuptools import setup
 
 setup(name = 'bevel',
-        version = '0.1.0',
+        version = '0.1.1',
         description = 'a selection of tools for viewing and editing bencoded entities',
         author = 'Igor Kaplounenko',
         author_email = 'igor@bittorrent.com',
         license = 'BSD',
         keywords = 'bevel bencode bittorrent utorrent',
         provides = ['bevel'],
+        requires = ['bencode (>=1.0)'],
         py_modules = ['bevel'],
         scripts = ['bcat', 'bgrep', 'bsed'],
         long_description = open('README').read()
